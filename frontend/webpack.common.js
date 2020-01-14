07656ce26fae20557e19077408b14c636646bba8
Tsuyoshi Fukuzawa <tsuyoshi.fukuzawa@gmail.com>
2020-01-14 16:22:45 +0900

    nodemonを入れる形で、dockerの構成に修正

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,9 +1,9 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
-  filename: "./index.html"
+  filename: "index.html"
 });
  module.exports = {
   entry: "./src/index.js",
