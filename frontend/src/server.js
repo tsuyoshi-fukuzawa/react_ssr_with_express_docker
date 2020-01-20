01e6ecc2fef0773bd97ccd445b24fe2c121bbe35
Tsuyoshi Fukuzawa <tsuyoshi.fukuzawa@gmail.com>
2020-01-20 15:01:04 +0900

    json出力

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,7 +1,31 @@
 import express from 'express';
+import log4js from 'log4js';
 import path from 'path';
 import { serverRoutes } from './routes/serverRoutes';
+
 const app = express();
+const logger = log4js.getLogger();
+
+// json出力にする
+log4js.addLayout('json',(config) => {
+  return (logEvent) => { 
+    return JSON.stringify(logEvent) + config.separator;
+  }
+});
+log4js.configure({
+  appenders: {
+    out: { type: 'stdout', layout: { type: 'json', separator: ',' } }
+  },
+  categories: {
+    default: { appenders: ['out'], level: 'info' }
+  }
+});
+
+
+// Expressのログ出力はlog4jsを使用する
+app.use(log4js.connectLogger(logger));
+
+// distを紐付ける
 app.use(express.static(path.join('./', 'dist')));
 
 // SSRに振りたいパスを記述していく
@@ -14,5 +38,5 @@ app.get('*', function (req, res) {
   res.sendFile(path.resolve('dist', 'index.html'));
 })
 app.listen(3000, ()=> {
-  console.log('server running');
+  console.log('server has been loaded');
 })
